eae86a8db28b0bcb8c932bb1c6362182eea11017
zhang.haipeng <646042371@qq.com>
2023-08-16 18:04:47 +0800

    【*】提交dome

---
+++ vue3-template/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MoviesCard msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import MoviesCard from "./components/MoviesCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    MoviesCard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
